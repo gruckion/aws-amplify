d7d07a662a2fb76da7708c43c20ebb3c5e71c308
Stephen Rayner <stephen.r.rayner@gmail.com>
2019-12-29 12:32:32 +0000

    Feature/basic todo list (#2)
    * created basic todo list component
    
    * Users can now create new notes
    
    * Users now fetch all notes on first load
    
    * Users can now delete  notes
    
    * Users can not edit notes
    
    * Set cursor
    
    * Adding types work in progress
    
    * moved types, added useDeepCompareEffect, ran amplify codegen and created useQuery hook
    
    * Switch to use the mutation abstraction
    
    * Safe guard against adding nothing
    
    * Adding notes  now reflects  on multiple clients
    
    * Nothing changed
    
    * Leave edit mode if note is deleted
    
    * Leave edit mode  if edited note is deleted
    
    * Deleting notes now works across clients
    
    * updating notes now reflects  on multiple clients
    
    * restricted access to owner of note, moved useQuery due to conflict with amplify
    
    * Clean up and refactor
    
    * renamed listeners
    
    * Added amplify add hosting for dev
    
    * amplify publish hosting
    
    * WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,216 @@
 import React from 'react';
 import { withAuthenticator } from "aws-amplify-react";
+import { API, graphqlOperation } from "aws-amplify";
+import { createNote, deleteNote, updateNote } from './graphql/mutations';
+import { listNotes } from './graphql/queries';
+import { mutation } from './hooks/useQuery';
+import { CreateNoteInput, CreateNoteMutation, DeleteNoteMutation, DeleteNoteInput, UpdateNoteMutation, UpdateNoteInput } from './API';
+import { onCreateNote, onDeleteNote, onUpdateNote } from './graphql/subscriptions';
+import Observable from 'zen-observable';
+
+interface Note {
+  id: string;
+  note: string;
+}
 
 const App: React.FC = () => {
+
+  const [note, setNote] = React.useState<string>("");
+  const [notes, setNotes] = React.useState<Note[]>([]);
+  const [id, setId] = React.useState<string>();
+
+  React.useEffect(() => {
+    (async () => {
+      await getNotes();
+    })();
+  }, []);
+
+  React.useEffect(() => {
+    let subscriptionListener!: ZenObservable.Subscription;
+    const subscription = API.graphql(graphqlOperation(onCreateNote));
+    console.log("subscription: ", subscription )
+    if(subscription instanceof Observable) {
+      subscriptionListener = subscription.subscribe({
+        next: noteData => {
+          const newNote = noteData.value.data.onCreateNote;
+          setNotes(prevNotes => {
+            if(prevNotes.findIndex(note => note.id === newNote.id) === -1) {
+              return [newNote, ...prevNotes];
+            }
+            return prevNotes;
+          });
+          console.log(newNote);
+        }
+      });
+    }
+    return () => {
+      subscriptionListener && subscriptionListener.unsubscribe();
+    }
+  }, []);
+
+  React.useEffect(() => {
+    let subscriptionListener!: ZenObservable.Subscription;
+    const subscription = API.graphql(graphqlOperation(onDeleteNote));
+    if(subscription instanceof Observable) {
+      subscriptionListener = subscription.subscribe({
+        next: noteData => {
+          const deletedNote = noteData.value.data.onDeleteNote;
+          setNotes(prevNotes => {
+            if(prevNotes.findIndex(note => note.id === deletedNote.id) > -1) {
+              return [...prevNotes.filter(note => note.id !== deletedNote.id)];
+            }
+            return prevNotes;
+          })
+        }
+      });
+    }
+    return () => {
+      subscriptionListener.unsubscribe();
+    }
+  });
+
+  React.useEffect(() => {
+    let subscriptionListener!: ZenObservable.Subscription;
+    const subscription = API.graphql(graphqlOperation(onUpdateNote));
+    if(subscription instanceof Observable) {
+      subscriptionListener = subscription.subscribe({
+        next: noteData => {
+          const updatedNote = noteData.value.data.onUpdateNote;
+          console.log("updatedNote: ", updatedNote);
+
+          setNotes(prevNotes => {
+            const index = notes.findIndex(note => note.id === updatedNote.id);
+            if(index !== -1) {
+              return [
+                ...notes.slice(0, index),
+                updatedNote,
+                ...notes.slice(index + 1)
+              ];
+            }
+            return prevNotes;
+          })
+        }
+      });
+    }
+    return () => {
+      subscriptionListener.unsubscribe();
+    }
+  });
+
+  const getNotes = async () => {
+    const result: any = await API.graphql(graphqlOperation(listNotes));
+    setNotes(result.data.listNotes.items);
+  }
+
+  const handleChangeNote = (event: React.ChangeEvent<HTMLInputElement>) => setNote(event?.target?.value);
+
+  const handleAddNote = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    if(!note) {
+      return;
+    }
+
+    const input = { note };
+    const data = await mutation<CreateNoteMutation, { input: CreateNoteInput }>(createNote, { input });
+    if(data && data.createNote) {
+      setNotes([data.createNote, ...notes]);
+    }
+    setNote("");
+  }
+
+  const handleDeleteNote = async (noteId: string) => {
+    const input = { id: noteId };
+
+    const data = await mutation<DeleteNoteMutation, { input: DeleteNoteInput }>(deleteNote, { input });
+    if(data && data.deleteNote) {
+      const deletedNoteId = data.deleteNote.id;
+      setNotes([...notes.filter(note => note.id !== deletedNoteId)]);
+
+      if (deletedNoteId === id) {
+        setId("");
+        setNote("");
+      }
+    }
+  }
+
+  const handleEditNoteMode = async (editNote: Note) => {
+    if(id) {
+      setId("");
+      setNote("");
+    } else {
+      console.log(editNote.id);
+      setId(editNote.id);
+      setNote(notes.filter(note => note.id === editNote.id)[0].note);
+    }
+  }
+
+  const handleUpdateNote = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const input: UpdateNoteInput = { id: id!, note };
+    const data = await mutation<UpdateNoteMutation, { input: UpdateNoteInput }>(updateNote, { input });
+    if(data && data.updateNote) {
+      const updatedNote = data.updateNote;
+      const index = notes.findIndex(note => note.id === updatedNote.id);
+      setNotes([
+        ...notes.slice(0, index),
+        updatedNote,
+        ...notes.slice(index + 1)
+      ]);
+      setNote("");
+      setId("");
+    }
+  }
+
+
   return (
-    <div>
-      App
+    <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
+      <h1 className="code f2-l">Amplify Notetaker</h1>
+      <form
+        className="mb3"
+        onSubmit={id ? handleUpdateNote : handleAddNote}
+        >
+        <input
+          type="text"
+          className="pa2 f4"
+          placeholder="Write your note"
+          onChange={handleChangeNote}
+          value={note}
+          />
+        <button
+          className="pa2 f4"
+          type="submit"
+          style={{ cursor: "pointer"}}
+        >
+          {id ? "Update Note" : "Add Note"}
+        </button>
+        <div className="flex flex-column items-center justify-center">
+          {
+            notes && notes.map(item => (
+              <div
+                key={item.id}
+                className="flex items-center"
+              >
+                <li
+                  className="list pa1 f3"
+                  onClick={() => handleEditNoteMode(item)}
+                  style={{ cursor: "pointer"}}
+                  >
+                  {item.note}
+                </li>
+                <button
+                  className="bg-transparent bn f4"
+                  type="button"
+                  onClick={() => handleDeleteNote(item.id)}
+                  style={{ cursor: "pointer"}}
+                >
+                  <span>&times;</span>
+                </button>
+              </div>))
+          }
+        </div>
+      </form>
     </div>
   );
 }
---
+++ src/hooks/useQuery.ts
@@ -0,0 +1,172 @@
+import React from "react";
+import { API, graphqlOperation } from "aws-amplify";
+import useDeepCompareEffect from "use-deep-compare-effect";
+import Observable from "zen-observable";
+
+type UseQueryType<ResultType> = {
+  loading: boolean;
+  error: any;
+  data: ResultType;
+  refetch: () => void;
+};
+
+export const mutation = async <
+  ResultType extends {},
+  VariablesType extends {} = {}
+>(
+  query: string,
+  variables?: VariablesType
+) => gqlOp<ResultType, VariablesType>(query, variables);
+
+export const gqlOp = async <
+  ResultType extends {},
+  VariablesType extends {} = {}
+>(
+  query: string,
+  variables?: VariablesType
+) => {
+  const { data } = (await API.graphql(graphqlOperation(query, variables))) as {
+    data: ResultType;
+  };
+  return data;
+};
+
+export const useQuery = <ResultType extends {}, VariablesType extends {} = {}>(
+  query: string,
+  variables?: VariablesType
+): UseQueryType<ResultType> => {
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState('');
+  const [data, setData] = React.useState({} as ResultType);
+
+  const fetchQuery = async (query: string, variables?: VariablesType) => {
+    try {
+      setLoading(true);
+      const data = await gqlOp<ResultType, VariablesType>(query, variables)
+      setData(data);
+    } catch (error) {
+      console.log(error);
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const refetch = () => {
+    fetchQuery(query, variables);
+  };
+
+  useDeepCompareEffect(() => {
+    fetchQuery(query, variables);
+  }, [query, variables]);
+
+  return {
+    loading,
+    data,
+    error,
+    refetch,
+  };
+};
+
+type ConfigType<VariableType extends {}> = {
+  query: string;
+  key: string;
+  variables?: VariableType;
+};
+
+export const useSubscription = <
+  ItemType extends { id: string },
+  VariablesType extends {} = {}
+>({
+  config,
+  itemData,
+}: {
+  config?: ConfigType<VariablesType>;
+  itemData?: ItemType;
+} = {}) => {
+  const [item, update] = React.useState<ItemType | undefined>(itemData);
+
+  React.useEffect(() => {
+    let unsubscribe;
+    if (config) {
+      const { query, key, variables } = config;
+      const subscription = API.graphql(graphqlOperation(query, variables));
+      if (subscription instanceof Observable) {
+        const sub = subscription.subscribe({
+          next: payload => {
+            try {
+              const {
+                value: {
+                  data: { [key]: item },
+                },
+              }: {
+                value: { data: { [key: string]: ItemType } };
+              } = payload;
+
+              update(item);
+            } catch (error) {
+              console.error(
+                `${error.message} - Check the key property: the current value is ${key}`
+              );
+            }
+          },
+        });
+        unsubscribe = () => {
+          sub.unsubscribe();
+        };
+      }
+    }
+    return unsubscribe;
+  }, [config]);
+
+  return [item];
+};
+
+export const useSubscriptionByItself = <
+  ItemType extends { id: string },
+  VariablesType extends {} = {}
+>({
+  config,
+  itemData,
+}: {
+  config?: ConfigType<VariablesType>;
+  itemData?: ItemType;
+} = {}) => {
+  const [item, update] = React.useState<ItemType | undefined>(itemData);
+
+  React.useEffect(() => {
+    let unsubscribe;
+    if (config) {
+      const { query, key, variables } = config;
+      const subscription = API.graphql(graphqlOperation(query, variables));
+      if (subscription instanceof Observable) {
+        const sub = subscription.subscribe({
+          next: payload => {
+            console.log("payload: ", payload);
+            try {
+              const {
+                value: {
+                  data: { [key]: item },
+                },
+              }: {
+                value: { data: { [key: string]: ItemType } };
+              } = payload;
+
+              update(item);
+            } catch (error) {
+              console.error(
+                `${error.message} - Check the key property: the current value is ${key}`
+              );
+            }
+          },
+        });
+        unsubscribe = () => {
+          sub.unsubscribe();
+        };
+      }
+    }
+    return unsubscribe;
+  }, [config]);
+
+  return [item];
+};
